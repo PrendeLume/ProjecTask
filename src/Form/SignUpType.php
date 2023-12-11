b5140d99db33739694304f9d85df2fea3c2c079f
Carlos <carlosalvarezsanchez17@gmail.com>
2023-12-11 13:42:24 +0100

    Maquetado SingUp y LogIn

--- src/Form/SignUpType.php
+++ src/Form/SignUpType.php
@@ -4,7 +4,11 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ColorType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,14 +17,46 @@ class SignUpType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('password')
-            ->add('name')
-            ->add('surname')
-            ->add('submit', SubmitType::class, [
-                'label' => 'Sign Up',
+            ->add('name', TextType::class, [
+                'label' => 'Nombre',
+                'label_attr' => [
+                    'class' => 'form-label'
+                ],
                 'attr' => [
-                    'class' => 'btn btn-primary'
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('surname', TextType::class, [
+                'label' => 'Apellido',
+                'label_attr' => [
+                    'class' => 'form-label'
+                ],
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Correo Electronico',
+                'label_attr' => [
+                    'class' => 'form-label'
+                ],
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('password', PasswordType::class, [
+                'label' => 'Contraseña',
+                'label_attr' => [
+                    'class' => 'form-label'
+                ],
+                'attr' => [
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Registrarse',
+                'attr' => [
+                    'class' => 'mt-2 btn btn-primary'
                 ]
             ])
         ;
