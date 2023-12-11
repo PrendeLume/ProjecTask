237ce4b4e30d2391cebf1d7817e9e79d7d8ebd74
Carlos <carlosalvarezsanchez17@gmail.com>
2023-12-11 09:06:40 +0100

    Creación del controlador de usuarios y preparación del cifrado de la contraseña

---
+++ src/Controller/UserController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractController
+{
+    #[Route('/index', name: 'app_index')]
+    public function index(): Response
+    {
+        return $this->render('user/index.html.twig', [
+            'controller_name' => 'UserController',
+        ]);
+    }
+    #[Route('/signup', name: 'app_signup')]
+    public function signup(UserPasswordHasherInterface $passwordHasher): Response
+    {
+        // get the user data from a registration form
+        $user = new User();
+        $plaintextPassword = $user->getPassword();
+
+        // hash the password (based on the security.yaml config for the $user class)
+        $hashedPassword = $passwordHasher->hashPassword(
+            $user,
+            $plaintextPassword
+        );
+        $user->setPassword($hashedPassword);
+        return $this->render('user/index.html.twig', [
+            'controller_name' => 'SignUp',
+        ]);
+    }
+}
