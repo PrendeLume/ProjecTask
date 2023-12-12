c1fee797447f5519c8b28f689ee3f433a7ba80ce
Carlos <carlosalvarezsanchez17@gmail.com>
2023-12-12 13:21:34 +0100

    Inicio de session al loguearse

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -2,20 +2,30 @@
 
 namespace App\Controller;
 
+
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 class LoginController extends AbstractController
 {
     #[Route('/login', name: 'app_login')]
-    public function index(AuthenticationUtils $authenticationUtils): Response
+    public function index(Request $request, AuthenticationUtils $authenticationUtils): Response
     {
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
 
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
+        if($this->getUser()){
+            $user = $this->getUser();
+            $session = $request->getSession();
+            $session->set('registro', $user->getUserIdentifier());
+            return $this->redirectToRoute('app_note');
+        }
+
 
         return $this->render('login/index.html.twig', [
             'last_username' => $lastUsername,
